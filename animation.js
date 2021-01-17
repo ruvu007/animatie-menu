5d1894bdb1454e774c27ca07805df0002bf965ea
Ruben V <ralvuurman@gmail.com>
2021-01-17 18:02:45 +0100

    Filmpje 6

--- animation.js
+++ animation.js
@@ -7,4 +7,16 @@ document.querySelector('.animeer-knop').addEventListener('click', (e) => {
 // na enige tijd na het laden van de pagina: start de animatie van het vlakje van zelf
 setTimeout( () => {
     document.querySelector('.vlakje').classList.toggle('vlakje__naar-rechts')    
-}, 2000 ) 
+}, 2000 );
+
+// een klik op nav__knop laat de nav__link (alle) na elkaar naar binnen schuiven
+// door ze de class nav__link--schuif-in te geven
+const menuKnop = document.querySelector('.nav__knop');
+const navItems = document.querySelectorAll('.nav__link');
+menuKnop.addEventListener('click', () => {
+    navItems.forEach( (item,index) => {
+        setTimeout( () => {
+            item.classList.toggle('nav__link--schuif-in');
+        }, 100 * index);
+    })
+}); 
