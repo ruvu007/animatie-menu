80457b8260348e340becc81fb432a240057ebb4f
Ruben V <ralvuurman@gmail.com>
2021-01-17 18:00:26 +0100

    Filmpje 5

--- animation.js
+++ animation.js
@@ -1,3 +1,10 @@
+//klik op animeer-knop laat vlakje animeren door class toe te voegen of weg te halen
 document.querySelector('.animeer-knop').addEventListener('click', (e) => {
     document.querySelector('.vlakje').classList.toggle('vlakje__naar-rechts')
-}) 
+});
+
+
+// na enige tijd na het laden van de pagina: start de animatie van het vlakje van zelf
+setTimeout( () => {
+    document.querySelector('.vlakje').classList.toggle('vlakje__naar-rechts')    
+}, 2000 ) 
